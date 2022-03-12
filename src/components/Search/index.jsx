2bcf865c49508aefc5a8947391c3ec4bcf4b1201
Negarnbkhti42 <norob35@gmail.com>
2022-03-12 14:07:21 +0330

    search display

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,22 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import "./search.scss";
 
 function Search() {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  const handleSearch = () => {
+    setOpen(true);
+    ref.current.focus();
+  };
+
+  const cancelSearch = () => {
+    setOpen(false);
+    setInput("");
+  };
 
   return (
     <div className="search_container">
-      <span className="search_icon">
+      <span className="search_icon" onClick={handleSearch}>
         <AiOutlineSearch />
       </span>
       <input
-        type="search"
+        type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-        className="search_input"
+        className={`search_input ${open && "search_input-open"}`}
         placeholder="search..."
+        ref={ref}
       />
+      {open && (
+        <button className="search_button" type="reset" onClick={cancelSearch}>
+          X
+        </button>
+      )}
     </div>
   );
 }
