cb85a2439f1912f089068edeeae49179dbf57981
Negarnbkhti42 <norob35@gmail.com>
2022-06-07 14:59:05 +0430

    react update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDomClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+ReactDomClient.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
