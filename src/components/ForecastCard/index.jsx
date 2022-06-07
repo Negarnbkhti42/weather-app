da241a29725935788c7703b149dfe800e4246f1f
Negarnbkhti42 <norob35@gmail.com>
2022-06-07 14:22:24 +0430

    Card prop set

--- src/components/ForecastCard/index.jsx
+++ src/components/ForecastCard/index.jsx
@@ -1,8 +1,9 @@
+import PropTypes from "prop-types";
 import "./forecastCard.scss";
 
-function ForecastCard({ hour, icon, condition }) {
+function ForecastCard({ hour, icon, condition, className }) {
   return (
-    <div className="card_container">
+    <div className={`card_container ${className}`}>
       <h4>{hour}</h4>
       <img src={icon} alt={`${condition} icon`} />
       <p>{condition}</p>
@@ -11,3 +12,17 @@ function ForecastCard({ hour, icon, condition }) {
 }
 
 export default ForecastCard;
+
+ForecastCard.propTypes = {
+  hour: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  icon: PropTypes.string,
+  condition: PropTypes.string,
+  className: PropTypes.string,
+};
+
+ForecastCard.defaultProps = {
+  hour: "na:na",
+  icon: "",
+  condition: "unknown",
+  className: "",
+};
